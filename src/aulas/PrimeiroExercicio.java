bf64c70c630fea52ceae2bb4bb959eb7611a9beb
RenanPaulino <renan.paulino@yahoo.com.br>
2022-08-08 05:30:53 -0300

    Initial commit

---
+++ src/aulas/PrimeiroExercicio.java
@@ -0,0 +1,35 @@
+package aulas;
+
+public class PrimeiroExercicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numero1;
+		numero1 = 3;
+		double numero2;
+		numero2 = 2;
+		System.out.println(numero1);
+		System.out.println(numero2);
+
+		String tipoString;
+		tipoString = "Renan";
+		System.out.println(tipoString);
+
+		char sexo;
+		sexo = 'M';
+		System.out.println(sexo);
+
+		boolean tipoLogico1, tipoLogico2;
+		tipoLogico1 = false;
+		tipoLogico2 = true;
+		System.out.println(tipoLogico1);
+		System.out.println(tipoLogico2);
+
+		String nome = "Renan Paulino";
+		int idade = 32;
+		double dinheiro = 1.25;
+
+		System.out.println(nome + " tem " + idade + " anos, e tem " + dinheiro + " em reais");
+	}
+}
